4f64896d99af15b929be58765384c0c9bfd5396e
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-13 16:18:53 -0300

    added syntax

--- 02-minutes-in-days.py
+++ 02-minutes-in-days.py
@@ -1,8 +1,9 @@
+calculo_de_segundos = 20 * 24 * 60 * 60 #declarar la variable  la antes 
+nombre_de_unidad = "segundos"
 print(f"20 days are {20 * 24 * 60} minutes")
-
-
-
-
+print(f"20 days are {20 * 24 * 60 * 60} {nombre_de_unidad}")
+print(f"20 days are {20 * calculo_de_segundos} segundos")
+print(f"25 days are {25 * calculo_de_segundos} segundos")
 #concatenacion de strings
 
 
