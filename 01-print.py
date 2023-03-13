a7c93f8a74d7efeed25db03b79642d65361fed16
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-13 15:49:21 -0300

    first commit

---
+++ 01-print.py
@@ -0,0 +1,6 @@
+print("amisauria")
+print(2)
+#todo entre  "" ´''es una string(str)
+#tipos de numeros :integer :numeros enteros positivo o negativo sin decimal 
+
+#tipos de numeros :float : numero positivo o negativo que contiene 1 o mas decimales
