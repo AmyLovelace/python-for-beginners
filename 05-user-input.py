0cc00bb6b8e5ef28ccdc47e494e08ee9200f76c6
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-16 12:49:44 -0300

    added a user input exercise

--- 05-user-input.py
+++ 05-user-input.py
@@ -1,1 +1,14 @@
-#user input exercise
+# user input exercise
+# calculo_de_segundos = 24 * 60 * 60 #declarar la variable  la antes
+calculo_de_minutos = 24 * 60
+nombre_de_unidad2 = " minutos"
+
+
+def dias_a_minutos(num_de_dias):
+    return(f"{num_de_dias} dias son {num_de_dias * calculo_de_minutos} {nombre_de_unidad2}")
+
+user_input=input("hola usuario, dame el numero de dias y los convertiré en horas\n")
+user_input_number = int(user_input)#volver una string en un numero/integer se le dice casting
+
+valor_calculado = dias_a_minutos(user_input_number)#asi el usuario entrega la informacion para que la variable lo calcule
+print(valor_calculado)
