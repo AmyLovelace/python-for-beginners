eae075d739a61e4e41be3d7b82a3ac5d3fb946e6
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-16 21:14:06 -0300

    list and sets

--- 13-sets_02.py
+++ 13-sets_02.py
@@ -1,1 +1,26 @@
 #conoce las caracteristicas de los sets
+
+my_set ={"Enero", "Febrero", "Marzo"}
+for element in my_set:
+    print(element)
+my_set.add("Abril")
+print(my_set)
+my_set.remove("Enero")
+print(my_set)
+
+my_list = ["Enero", "Febrero", "Marzo","Octubre"]
+my_list.remove("Enero")
+print(my_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
