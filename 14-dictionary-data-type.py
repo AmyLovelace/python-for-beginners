6f32039815472c88b09083a09908c3695bf3c691
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-17 12:09:40 -0300

    added a dictionary exercise

--- 14-dictionary-data-type.py
+++ 14-dictionary-data-type.py
@@ -1,17 +1,18 @@
 #ejercicio sets
  
-calculo_de_minutos = 24 * 60
-nombre_de_unidad2 ='minutos'
 
-
-def dias_a_minutos(num_de_dias):
-    return f"{num_de_dias} dias son {num_de_dias * calculo_de_minutos} {nombre_de_unidad2}"
-  
+def dias_a_minutos(num_de_dias, unidad_conversion):
+    if unidad_conversion == "minutos":
+        return f"{num_de_dias} dias son {num_de_dias * 1440} minutos"
+    elif unidad_conversion == "horas":
+        return f"{num_de_dias} dias son {num_de_dias * 24} horas"
+    else:
+        return "unidad no valida" 
 def validar_y_ejecutar():
     try:
-        user_input_number = int(num_de_dias_elemento)
+        user_input_number = int(dias_y_unidad_dictionary["dias"])
         if user_input_number > 0:
-            valor_calculado = dias_a_minutos(user_input_number)#asi el usuario entrega la informacion para que la variable lo calcule
+            valor_calculado = dias_a_minutos(user_input_number, dias_y_unidad_dictionary["unidad"])#asi el usuario entrega la informacion para que la variable lo calcule
             print(valor_calculado)
         elif user_input_number == 0:
             print("entregaste un 0, porfavor digita un numero positivo")
@@ -22,13 +23,9 @@ def validar_y_ejecutar():
 
 user_input =""
 while user_input != "salir":
-    user_input=input("hola usuario, dame el numero de dias y los convertiré en minutos\n")
-    lista_de_dias = user_input.split(", ")
-    print(lista_de_dias)
-    print (set(lista_de_dias))
-
-    print(type(lista_de_dias))
-    print(type(set(lista_de_dias)))
-    
-    for num_de_dias_elemento in set(lista_de_dias):
-        validar_y_ejecutar()
+    user_input=input("hola usuario, dame el numero de dias y la unidad de conversion\n")
+    dias_y_unidad = user_input.split(":")
+    print(dias_y_unidad)
+    dias_y_unidad_dictionary = {"dias":dias_y_unidad[0], "unidad": dias_y_unidad[1]} 
+    print(dias_y_unidad_dictionary)
+    validar_y_ejecutar()
