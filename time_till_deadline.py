f377febba65efafb0ef9e17fc864947d7588e70e
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-17 17:06:25 -0300

    made a deadline calculator

---
+++ time_till_deadline.py
@@ -0,0 +1,18 @@
+import datetime
+
+user_input=input("enter your goal with a deadline separated by colon\n")
+input_list = user_input.split(":")
+
+goal = input_list[0]
+deadline = input_list[1]
+
+dateline_date = (datetime.datetime.strptime(deadline, "%d.%m.%Y"))
+today_date = datetime.datetime.today()
+time_till = dateline_date - today_date
+
+hours_till = int(time_till.total_seconds()/60/60)
+#calcula cuantos dias para la meta
+
+print(f"Dear user! Time remaining for your goal :{goal} is {hours_till} horas.")
+print(f"Dear user! Time remaining for your goal :{goal} is {time_till.days} days.")
+
