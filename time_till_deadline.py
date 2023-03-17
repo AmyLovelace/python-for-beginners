5cc61e4afebf3a23fa45d2d1e3e91788cc584c59
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-17 17:11:35 -0300

    added till deadline calculator

--- time_till_deadline.py
+++ time_till_deadline.py
@@ -13,6 +13,6 @@ time_till = dateline_date - today_date
 hours_till = int(time_till.total_seconds()/60/60)
 #calcula cuantos dias para la meta
 
-print(f"Dear user! Time remaining for your goal :{goal} is {hours_till} horas.")
+print(f"Dear user! Time remaining for your goal :{goal} is {hours_till} hours.")
 print(f"Dear user! Time remaining for your goal :{goal} is {time_till.days} days.")
 
