1de856f169f5f6f9cb434527c55114a2fc26842e
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-20 17:03:07 -0300

    added a classes and objects oop exercises

--- classes&objects/main.py
+++ classes&objects/main.py
@@ -1,6 +1,10 @@
 from user import User
+from post import Post
 app_user_one = User("amy@gmail.com", " ami sauria", " ami666", "developer")
 app_user_one.get_user_info()
 
 app_user_two=User("adel@gmail.com"," adelin bombin","hermanoso profesional","jedi")
 app_user_two.get_user_info()
+
+new_post=Post("on a mission to be the baddest bitch alive", app_user_two.name)
+new_post.get_post_info()
