abeb5614ea8c998f9c379225db03b3fdf7da8967
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-20 16:45:02 -0300

    first commit

--- classes&objects/user.py
+++ classes&objects/user.py
@@ -17,9 +17,4 @@ class User:
             f"user{self.name} currently works as a {self.current_job_title}. you can contact them at {self.email}")
 
 
-app_user_one = User("amy@gmail.com", " ami sauria", " ami666", "developer")
-app_user_one.get_user_info()
-
-app_user_two=User("adel@gmail.com"," adelin bombin","hermanoso profesional","jedi")
-app_user_two.get_user_info()
 
